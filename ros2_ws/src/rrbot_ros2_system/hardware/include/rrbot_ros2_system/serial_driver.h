42beebcc669c2e693f8fb2012c70a375887b97af
kiinging <wongkiinging@gmail..com>
2025-06-25 16:12:02 +0800

    Re-add rrbot_ros2_system as regular directory

---
+++ ros2_ws/src/rrbot_ros2_system/hardware/include/rrbot_ros2_system/serial_driver.h
@@ -0,0 +1,34 @@
+
+#ifndef RRBOT_ROS2_SYSTEM_SERIAL_DRIVER_H
+#define RRBOT_ROS2_SYSTEM_SERIAL_DRIVER_H
+
+#include <libserial/SerialPort.h>
+
+#include <iostream>
+#include <string>
+#include <utility>
+
+using namespace LibSerial;
+
+class SerialDriver
+{
+public:
+    SerialDriver();
+    SerialDriver(std::string port_name, int baudrate);
+    void configure(std::string port_name="/dev/ttyACM0", int baudrate=115200, int timeout_ms=20);
+    LibSerial::BaudRate convertBaudrate(int baudrate);
+    std::string readLine();
+    void writeLine(std::string line);
+    std::pair<int, double> getServoState(uint8_t nservo);
+    bool connected();
+    void close();
+    
+private:
+    SerialPort  _serial_conn;
+    int         _timeout_ms;
+    bool        _connected;
+    std::string _port_name;
+    uint        _baud_rate;
+};
+
+#endif
